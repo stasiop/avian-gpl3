7fbb7a71a288547ae5567383a5001f8793e90db7
JohnBena <77627847+JohnBena@users.noreply.github.com>
2021-01-29 17:40:11 +0000

    Update anonchat.php

--- anonchat.php
+++ anonchat.php
@@ -1,4 +1,5 @@
 
+
 <html>
         <head></head>
         <title>AnonChat</title>
@@ -8,41 +9,45 @@
                         <img class="direction" src="/src/images/anon chat neon.png" width="320" height="200">
                         <div class="textContainer">
                                 <?php      
-                                      $i=true;
-                                      while($i){
+                                
+                                      
+                                      function saveFile(){
+                                        $IDstr = file_get_contents("src/messageHandler/id.txt");
+                                        $ID = (int) $IDstr;
+                                        $name = "Anon";
+                                        $ID++;
+                                        
+                                        $data_file = fopen("message.txt", "a+");
+                                        $IDsave = fopen("src/messageHandler/id.txt", "w+");
+                                        $message = $_POST["message"];
+                                        $text_to_write = $name . ": " . $message . "\n";
+                                        fwrite($IDsave, $ID);
+                                        fwrite($data_file, $text_to_write);
+                                        fclose($data_file);
+                                        header('Location: /anonchat.php');
+                                        fwrite($IDsave, $ID);
+                                       }
+                                ?>
+                                <?php
+                                        $serverID = file_get_contents("src/messageHandler/id.txt");
+                                ?>			
+                                
+                                <script> 
+                                        var serverId = "<?php echo $serverID; ?>";
+                                        var clientId=0;
+                                        var x=true;
+                                        while(x){
+                                                if(serverId!=clientId){
+                                                        clientId=serverId;
+                                                        saveFile();
+                                                }
+                                        }
+                                </script>
 
-                                          function saveFile(){
-                                            $IDstr = file_get_contents("src/messageHandler/id.txt");
-                                            $ID = (int) $IDstr;
-                                            $name = "Anon";
-                                            $ID++;
-                                            
-                                            $data_file = fopen("message.txt", "a+");
-                                            $IDsave = fopen("id.txt", "w+");
-                                            $message = $_POST["message"];
-                                            $text_to_write = $name . ": " . $message . "\n";
-                                            fwrite($IDsave, $ID);
-                                            fwrite($data_file, $text_to_write);
-                                            fclose($data_file);
-                                            header('Location: /anonchat.php');
-                                            fwrite($IDsave, $ID);
-                                          }
-                                          $x = 0;
-
-                                          $UFID = file_get_contents("src/messageHandler/id.txt");
-                                          $serverID=(int) $UFID;
-
-                                          if($UFID!=$x){
-                                            $x=$UFID;
-                                            saveFile();
-                                          }
-                                          sleep(1);
-
-                                    }
 
 
                                     
-                                ?>
+                                
                                       <iframe id="GFG" src = "/src/messageHandler/message.txt" width = "100%" height = "100%"style="border: 0px">
                                         Sorry your browser does not support inline frames. Use GNU IceCat.
                                       </iframe>
@@ -75,6 +80,7 @@
                                 <input name="message" placeholder="Isn't avian so pog?!" type="text">
                                 <button type="submit" value="submit">Send</button>
                         </form>
+                      
                 </div>
         </body>
 
