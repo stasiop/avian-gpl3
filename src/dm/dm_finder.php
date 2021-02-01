f6169f03faeb0bb64b30c751f61b7bec53cc4a9d
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-01 13:28:25 +0000

    Update dm_finder.php

--- src/dm/dm_finder.php
+++ src/dm/dm_finder.php
@@ -5,9 +5,13 @@ if (file_exists($DMnum . ".txt")) {
 	$cookie_value = $DMnum;
 	setcookie($cookie_name, $cookie_value, time() + (10), "/"); // 86400 = 1 day
 ?>
-	<iframe id="GFG" src = "/src/dm/<?php echo $DMnum; ?>.txt" width = "100%" height = "100%"style="border: 0px">
+	<iframe id="GFG" src = "/src/dm/<?php echo $DMnum; ?>.txt" width = "50%" height = "50%"style="border: 0px">
 		Sorry your browser does not support inline frames. Use GNU IceCat.
     </iframe>
+	<form method="POST" action="/src/dm/submitDM.php">
+        <input name="message" placeholder="Isn't avian so pog?!" type="text">
+        <button type="submit" value="submit">Send</button>
+    </form>
 <?php
 } else { echo "Are you sure that you typed your id correctly? The id you typed: " . $DMnum; }
 ?>
