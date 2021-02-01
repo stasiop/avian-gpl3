ae3f649d368cc85d21c83afd37c22402fb2b3c28
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-01 14:40:51 +0000

    Update accountScreen.php

--- accountScreen.php
+++ accountScreen.php
@@ -1,20 +1,17 @@
 <?php
-//if(!isset($_COOKIE["USER FOR DM"])) {
-//setcookie("USER FOR DM", "A", time() + (86400 * 30), "/src/dm/");
-//}
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+if(!isset($_COOKIE["user"])) {
+	$cookie_name = "user";
+	$cookie_value = rand(0, 32677);
+	setcookie($cookie_name, $cookie_value, time() + (86400 * 365), "/"); // 86400 = 1 day
+}
 ?>
 <table width="10%" border="1" class="table">
     <tr>
         <td><a href="/anonchat.php">anonchat</a></td>
+		<td><a href="/src/dm/dm_generator.php">Private DM w/ stan</a></td>
     </tr>
 </table>
-<!-- FIGURE OUT HOW TO ADD TABLE BLOCK FOR EACH SERVER THEY ARE IN --!>
-<table width="10%" border="1" class="table">
-    <tr>
-        <td><a href="/src/dm/dm_generator.php">Private DM w/ stan</a></td>
-    </tr>
-</table>
-
+<form method="POST" action="/src/dm/dm_finder.php">
+	<input name="DMnum" placeholder="12345" type="text">
+	<button type="submit" value="submit">go to dm</button>
+</form>
