f818e5d2bbedb26faf685dc5e550426c71a982bd
JohnBena <77627847+JohnBena@users.noreply.github.com>
2021-02-01 14:17:08 +0000

    Update anonchat.php

--- anonchat.php
+++ anonchat.php
@@ -1,6 +1,4 @@
 
-
-
 <html>
         <head></head>
         <title>AnonChat</title>
@@ -9,45 +7,51 @@
                         <link type="text/css" rel="stylesheet" href="Stylesheet.css">
                         <img class="direction" src="/src/images/anon chat neon.png" width="320" height="200">
                         <div class="textContainer">
-                               
-                                <?php
-                                        $serverID = file_get_contents("src/messageHandler/id.txt");
-                                ?>			
-                                
-                                <script> 
-                                        var serverId = "<?php echo $serverID; ?>";
+                                <!--
+                                        
+                                        //while(TRUE){
+                                        //        $serverID = file_get_contents("src/messageHandler/id.txt");
+                                        //        sleep(1);
+                                        //}
+                                -->		
+                                	
+                                <script type="text/javascript">        
+                                        
                                         var clientId=0;
                                         var x=true;
-                                        while(x){
-                                                if(serverId!=clientId){
-                                                        clientId=serverId;
-                                                        window.location.reload(true);
+                                        function loadDoc() {
+                                                var xhttp = new XMLHttpRequest();
+                                                xhttp.onreadystatechange = function() {
+                                                if (this.readyState == 4 && this.status == 200) {
+                                                        $serverID = this.responseText;
                                                 }
-                                                sleep(1);
+                                                };
+                                                        xhttp.open("GET", "/src/messageHandler/id.txt", true);
+                                                        xhttp.send();
+                                                }
+                                        const task = async () => {
+                                                while(x) {
+                                                        loadDoc()
+                                                        
+                                                        await new Promise(r => setTimeout(r, 1000));
+                                                        if(clientId!=serverId){
+                                                        
+                                                                console.log("differentID")
+                                                                clientId=serverId;
+                                                                window.location.reload()
+                                                                
+                                                        }else{
+                                                                console.log("sameID");
+                                                        };
+                                                        
+                                                };
                                         }
-                                </script>
-
-
-
-                                    
-                                
+                                        
+                                </script>                         
                                       <iframe id="GFG" src = "/src/messageHandler/message.txt" width = "100%" height = "100%"style="border: 0px">
                                         Sorry your browser does not support inline frames. Use GNU IceCat.
                                       </iframe>
-
-                                      
-
                         </div>
-
-                      
-
-                            <script>
-                                Autoscroll();
-                            </script>
-
-
-
-
                             <script>
                                 function Autoscroll() { // this is good
                                 var iframeID = document.getElementById("GFG");
@@ -58,6 +62,9 @@
                                 iframeCW.body.style.color = "white";
                                 }
                            </script>
+                           <script>
+                                Autoscroll();
+                            </script>
                         <form method="POST" action="src/messageHandler/submit.php">
 
                                 <input name="message" placeholder="Isn't avian so pog?!" type="text">
