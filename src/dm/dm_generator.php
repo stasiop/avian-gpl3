2b6eb52c825c2aecc7d0ebb1ba3491e18f6f8276
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-20 23:05:09 +0000

    Update dm_generator.php

--- src/dm/dm_generator.php
+++ src/dm/dm_generator.php
@@ -2,17 +2,18 @@
 session_start();
 $_SESSION["DMnum"] = $_POST["DMnum"];
 if(!isset($_COOKIE["user"])) {
-	echo "YOU FOOL, YOUR COOKIE ISN'T SET";
+        echo "YOU FOOL, YOUR COOKIE ISN'T SET";
 } else {
-	if (file_exists($_COOKIE["user"] . ".txt")) {
- 		echo "The file exists";
-	} else {
-		echo "doesn't";
-		$myfile = fopen($_COOKIE["user"] . ".txt", "a+") or die("\nUnable to open file!");
-		$txt = " ";
-		fwrite($myfile, $txt);
-		fclose($myfile);
-	}
+        if (file_exists($_COOKIE["user"] . ".txt")) {
+                echo "The file exists";
+        } else {
+                echo "doesn't exist (";
+                echo $_COOKIE["user"] . ".txt)\n";
+                $myfile = fopen($_COOKIE["user"] . ".txt", "a+") or die("\nUnable to create file!");
+                $txt = " ";
+                fwrite($myfile, $txt);
+                fclose($myfile);
+        }
 }
-header ('Location: ' . $_COOKIE["user"] . ".txt"); 
+header ('Location: ' . $_COOKIE["user"] . ".txt");
 ?>
